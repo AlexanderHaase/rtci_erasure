fc866128cfa9651c62d94cbc0480cf46654218b7
Alexander Haase <alex@alexanderhaase.net>
2019-01-19 09:41:48 -0500

    WIP on template collection rework

--- type.cpp
+++ type.cpp
@@ -9,6 +9,12 @@ namespace rtci {
     {
       struct empty {};
 
+      template < typename Type >
+      struct identity { using type = Type; };
+
+      template < typename Type >
+      using unwrap = typename Type::type;
+
       namespace detail
       {
         /// More flexible variant of void_t
@@ -86,13 +92,23 @@ namespace rtci {
       template < typename ... Types>
       struct is_iterator<iterator<Types...>> : std::true_type {};
 
-      template < typename Iterator, int Amount >
-      struct seek
-      { 
-        using step = typename std::conditional_t<(Amount < 0), maybe<prev, Iterator>, maybe<next, Iterator>>::type;
-        static constexpr int increment = (Amount < 0 ? -1 : 1);
-        using type = typename seek<step, (Amount - increment)>::type;
-      };
+      namespace detail
+      {
+        template < typename Type, typename = void >
+        struct make_iterable;
+
+        template < typename Type >
+        struct make_iterable<Type, std::enable_if_t<is_iterator<Type>::value>> { using type = Type; };
+
+        template < typename Type >
+        struct make_iterable<Type, std::enable_if_t<has_op<begin, Type>::value>> : begin<Type> {};
+      }
+
+      template < typename Type >
+      struct iterate : detail::make_iterable<Type> {};
+
+      template < typename Type >
+      using iterate_t = typename iterate<Type>::type;
 
       namespace detail
       {
@@ -135,6 +151,14 @@ namespace rtci {
 
       /// Seek operation on iterators
       ///
+      template < typename Iterator, int Amount >
+      struct seek
+      { 
+        using step = unwrap<std::conditional_t<(Amount < 0), maybe<prev, Iterator>, maybe<next, Iterator>>>;
+        static constexpr int increment = (Amount < 0 ? -1 : 1);
+        using type = unwrap<seek<step, (Amount - increment)>>;
+      };
+
       template < typename Iterator>
       struct seek<Iterator, 0> { using type = Iterator; };
 
@@ -143,30 +167,82 @@ namespace rtci {
 
       /// Reverse implementation
       ///
-      template < typename Iterator >
-      struct reverse;
-
-      /// Work around for clang template limitations
-      ///
-      using reverse_type = template_type<reverse>;
+      struct reverse_tag {};
 
       template < typename Iterator >
-      struct reverse { using type = iterator<reverse_type, Iterator, void>; };
+      struct reverse { using type = iterator<reverse_tag, iterate_t<Iterator>, void>; };
 
       template < typename Iterator >
       using reverse_t = typename reverse<Iterator>::type;
 
       template < typename Iterator >
-      struct next<iterator<template_type<reverse>, Iterator, std::enable_if_t<has_op<prev, Iterator>::value>>>
-      { using type = iterator<template_type<reverse>, prev_t<Iterator>, void>; };
+      struct next<iterator<reverse_tag, Iterator, std::enable_if_t<has_op<prev, Iterator>::value>>>
+      { using type = iterator<reverse_tag, prev_t<Iterator>, void>; };
 
       template < typename Iterator >
-      struct prev<iterator<template_type<reverse>, Iterator, std::enable_if_t<has_op<next, Iterator>::value>>>
-      { using type = iterator<template_type<reverse>, next_t<Iterator>, void>; };
+      struct prev<iterator<reverse_tag, Iterator, std::enable_if_t<has_op<next, Iterator>::value>>>
+      { using type = iterator<reverse_tag, next_t<Iterator>, void>; };
 
       template < typename Iterator >
-      struct eval<iterator<template_type<reverse>, Iterator, std::enable_if_t<has_op<eval, Iterator>::value>>>
+      struct eval<iterator<reverse_tag, Iterator, std::enable_if_t<has_op<eval, Iterator>::value>>>
       { using type = eval_t<Iterator>; };
+
+      /// Map implementation
+      ///
+      struct map_tag {};
+
+      template < template < typename > class Op, typename Iterator >
+      struct map { using type = iterator< map_tag, template_type<Op>, iterate_t<Iterator>, void >; };
+
+      template < template < typename > class Op, typename Iterator >
+      using map_t = typename map<Op, Iterator>::type;
+
+      template < template < typename > class Op, typename Iterator >
+      struct next<iterator<map_tag, template_type<Op>, Iterator, std::enable_if_t<has_op<next, Iterator>::value>>>
+      { using type = iterator<map_tag, template_type<Op>, next_t<Iterator>, void>; };
+
+      template < template < typename > class Op, typename Iterator >
+      struct prev<iterator<map_tag, template_type<Op>, Iterator, std::enable_if_t<has_op<prev, Iterator>::value>>>
+      { using type = iterator<map_tag, template_type<Op>, prev_t<Iterator>, void>; };
+
+      template < template < typename > class Op, typename Iterator >
+      struct eval<iterator<map_tag, template_type<Op>, Iterator, std::enable_if_t<has_op<eval, Iterator>::value>>>
+      { using type = Op<eval_t<Iterator>>; };
+
+      /// Repeat implementation
+      ///
+      template < template < typename > class Op, typename Init, size_t N >
+      struct repeat
+      {
+        template < typename Type >
+        using recurse = repeat<Op, Op<Type>, (N-1)>;
+        using type = unwrap<std::conditional_t<(N==0), identity<Init>, maybe<recurse, Init>>>;
+      };
+
+      template < template < typename > class Op, typename Init, size_t N >
+      using repeat_t = unwrap<repeat<Op, Init, N>>;
+
+      template < template < typename > class Op, typename Init, template < typename > class Predicate >
+      struct repeat_until
+      {
+          template < typename Type >
+          using recurse = repeat_until<Op, Op<Type>, Predicate>;
+          using type = typename std::conditional_t< Predicate<Init>::value, identity<Init>, maybe<recurse, Init>>::type;
+      };
+
+      template < template < typename > class Op, typename Init, template < typename > class Predicate >
+      using repeat_until_t = unwrap<repeat_until<Op, Init, Predicate>>;
+
+      /// Filter implementation
+      ///
+      struct filter_tag {};
+
+      template < template < typename > class Op, typename Iterator >
+      struct filter { using type = iterator< filter_tag, template_type<Op>, iterate_t<Iterator>, void, void >; };
+
+      template < template < typename > class Op, typename Iterator >
+      using filter_t = typename filter<Op, Iterator>::type;
+
 
       /// List implementation
       ///
@@ -198,11 +274,13 @@ namespace rtci {
       static_assert( has_op<eval, begin_t<list<int>>>::value, "check has_op<eval, ...>" );
       static_assert( has_op<next, begin_t<list<int>>>::value, "check has_op<eval, ...>" );
       static_assert( has_op<prev, end_t<list<int>>>::value, "check has_op<eval, ...>" );
+      static_assert( std::is_same<repeat_t<next_t, begin_t<list<int, int>>, 2>, end_t<list<int, int>>>::value, "check repeat" );
+      //static_assert( std::is_same<repeat_until_t<next_t, begin_t<list<int, int>>,
       static_assert( std::is_same<seek_t<begin_t<list<int, int>>,2>, end_t<list<int, int>>>::value, "check seek forward" );
       static_assert( std::is_same<seek_t<end_t<list<int, int>>,-2>, begin_t<list<int, int>>>::value, "check seek reverse" );
       static_assert( length<list<int, int>>::value == 2, "check length" );
-      static_assert( std::is_same<eval_t<next_t<next_t<reverse_t<end_t<list<int, int>>>>>>, int>::value, "check reverse eval" );
       static_assert( length<reverse_t<end_t<list<int, int>>>>::value == 2, "check reverse iteration" );
+      static_assert( eval_t<map_t<is_iterator, list<iterator<>>>>::value, "check map eval" );
     }
   }
 }
